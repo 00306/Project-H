7ac32dd03233d274ab8cfef7c2c4b9387b13bfea
Eric <real.dassy@gmail.com>
2024-05-11 00:26:16 +0900

    Initial Commit

---
+++ ProjectH/ProjectHApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectHApp.swift
+//  ProjectH
+//
+//  Created by 송지혁 on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
