c668e628a5926566fa56b40600431196841483df
Eric <real.dassy@gmail.com>
2024-05-11 01:31:01 +0900

    feat: AuthManager 구현
    인증 기능을 관리하는 AuthManager를 구현했습니다.
    - 싱글톤 패턴으로 단 하나의 인스턴스만 생성되도록 보장
    - signIn 함수 구현
    - signUp 함수 구현

---
+++ ProjectH/Managers/AuthManager.swift
@@ -0,0 +1,38 @@
+//
+//  AuthManager.swift
+//  ProjectH
+//
+//  Created by 송지혁 on 5/11/24.
+//
+
+import FirebaseAuth
+import Foundation
+
+enum AuthError: Error {
+    case userNotFound
+    case wrongPassword
+}
+
+class AuthManager {
+    static let shared = AuthManager()
+    let auth = Auth.auth()
+    var user: User? = nil
+    
+    func signIn(email: String, password: String) async {
+        do {
+            let result = try await auth.signIn(withEmail: email, password: password)
+            self.user = result.user
+        } catch {
+            print(error)
+        }
+    }
+    
+    func signUp(email: String, password: String) async {
+        do {
+            let _ = try await auth.createUser(withEmail: email, password: password)
+        } catch {
+            print(error)
+        }
+    }
+}
+
