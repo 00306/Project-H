b3707f3661df20b679a4f517d7f29f3a5d983680
Eric <real.dassy@gmail.com>
2024-05-11 00:31:29 +0900

    first commit

---
+++ ProjectH/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ProjectH
+//
+//  Created by 송지혁 on 5/11/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
