f544286be157f0f6ff8964c59e31b348448219bf
Eric <real.dassy@gmail.com>
2024-05-15 21:26:38 +0900

    test: 테스트를 위한 MainView 구현
    MainViewModel의 로직을 테스트해보기 위한 예시 View를 구현했습니다.

--- ProjectH/Views/MainView.swift
+++ ProjectH/Views/MainView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct MainView: View {
     let authService: AuthService
+    let mainViewModel = MainViewModel()
     
     init(authService: AuthService = AuthManager.shared) {
         self.authService = authService
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
-        Button("로그아웃") {
-            authService.signOut()
+        VStack {
+            ForEach(mainViewModel.hackathons, id: \.self) { hackathon in
+                Text(hackathon.id!)
+                Text(hackathon.name)
+                Text(hackathon.description)
+            }
+            
+            Button("파이어스토어") {
+                mainViewModel.fetchHackathons()
+            }
+            
+            Button("로그아웃") {
+                authService.signOut()
+            }
         }
     }
 }
