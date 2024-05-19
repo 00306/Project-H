2fdb73690b026aa02108f39bf13d8300bae2c144
Eric <real.dassy@gmail.com>
2024-05-19 22:17:19 +0900

    refactor: body diet
    컴포넌트 단위로 쪼갤 수 있는 뷰들을 연산 프로퍼티로 선언하여 body의 구조를 명확히 했습니다.

--- ProjectH/Views/MainView.swift
+++ ProjectH/Views/MainView.swift
@@ -9,26 +9,34 @@ import SwiftUI
 
 struct MainView: View {
     let authService: AuthService
-    let mainViewModel = MainViewModel()
+    @StateObject var mainViewModel = MainViewModel()
     
     init(authService: AuthService = AuthManager.shared) {
         self.authService = authService
     }
     
     var body: some View {
-        VStack {
-            ForEach(mainViewModel.hackathons, id: \.self) { hackathon in
-                Text(hackathon.id!)
-                Text(hackathon.name)
-                Text(hackathon.description)
+        NavigationStack {
+            VStack {
+                hackathonlist
+                
+                Button("파이어스토어") {
+                    mainViewModel.fetchHackathons()
+                }
+                
+                Button("로그아웃") {
+                    authService.signOut()
+                }
             }
-            
-            Button("파이어스토어") {
-                mainViewModel.fetchHackathons()
-            }
-            
-            Button("로그아웃") {
-                authService.signOut()
+        }
+    }
+    
+    private var hackathonlist: some View {
+        ForEach($mainViewModel.hackathons) { $hackathon in
+            NavigationLink {
+                HackathonDetailView(hackathon: hackathon)
+            } label: {
+                HackathonComponent(hackathon: $hackathon, mainViewModel: mainViewModel)
             }
         }
     }
