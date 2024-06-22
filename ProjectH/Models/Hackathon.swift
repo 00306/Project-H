d4870797f762bed779d107702d5ee9574e04e948
Eric <real.dassy@gmail.com>
2024-06-23 01:08:45 +0900

    feat: Codable 프로토콜 코드 구현
    모델의 타입이 class로 바뀜에 따라 Codable(Decodable&Encodable) 프로토콜을 만족시키기 위해 다음과 같은 사항을 추가했습니다.
    - decode를 위한 필요 생성자 추가
    - encode를 위한 encode 함수 추가

--- ProjectH/Models/Hackathon.swift
+++ ProjectH/Models/Hackathon.swift
@@ -16,11 +16,36 @@ class Hackathon: Identifiable, Codable, Hashable {
     let content: String
     var bookmarks: Int
     let imageUrl: String
-}
+    
+    enum CodingKeys: CodingKey {
+        case id, name, content, bookmarks, imageUrl
+    }
+    
     init(id: String? = nil, name: String, content: String, bookmarks: Int, imageUrl: String) {
         self.id = id
         self.name = name
         self.content = content
         self.bookmarks = bookmarks
         self.imageUrl = imageUrl
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try? container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.content = try container.decode(String.self, forKey: .content)
+        self.bookmarks = try container.decode(Int.self, forKey: .bookmarks)
+        self.imageUrl = try container.decode(String.self, forKey: .imageUrl)
+        
+        
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(content, forKey: .content)
+        try container.encode(bookmarks, forKey: .bookmarks)
+        try container.encode(imageUrl, forKey: .imageUrl)
+        
     }
