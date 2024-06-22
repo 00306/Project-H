bd7efefdb6e3d55cf9b6eb5a9e7874918f5d2bb7
Eric <real.dassy@gmail.com>
2024-06-23 01:16:06 +0900

    feat: UserModel bookmark 관리 기능 추가
    SwiftData 기능의 추가로 인해 이제부터 UserModel이 bookmark Array를 관리합니다.
    - SwiftData ModelContext Injection
    - SwiftData에 데이터를 추가하는 update 함수 추가. 추후 네이밍 변경 예정
    - SwiftData 데이터베이스 리스트를 fetch하는 fetchSwiftData 함수 추가. 추후 네이밍 변경 예정

--- ProjectH/Models/UserModel.swift
+++ ProjectH/Models/UserModel.swift
@@ -8,19 +8,39 @@
 import Combine
 import Firebase
 import Foundation
+import SwiftData
 
 final class UserModel: ObservableObject {
-    let authManager: AuthManager
+    let authManager: FirebaseAuthService
     @Published private(set) var user: UserInfo?
     @Published private(set) var authenticationState: AuthenticationState = .unauthenticated
+    @Published var bookmarks: [Hackathon] = []
     
-    private var firestoreManager: FirestoreManager
+    private var firestoreManager: FirestoreService
     private var cancellables = Set<AnyCancellable>()
+    private var modelContext: ModelContext
     
-    init(firestoreManager: FirestoreManager = FirestoreManager.shared, authManager: AuthManager = AuthManager.shared) {
+    init(firestoreManager: FirestoreService = FirestoreService.shared, authManager: FirebaseAuthService = FirebaseAuthService.shared, modelContext: ModelContext) {
+        self.modelContext = modelContext
         self.firestoreManager = firestoreManager
         self.authManager = authManager
         self.observeAuthState()
+        
+        fetchSwiftData()
+    }
+    
+    func update(bookmark: Hackathon) {
+        modelContext.insert(bookmark)
+        fetchSwiftData()
+    }
+    
+    func fetchSwiftData() {
+        do {
+            let descriptor = FetchDescriptor<Hackathon>(sortBy: [SortDescriptor(\.id)])
+            self.bookmarks = try modelContext.fetch(descriptor)
+        } catch {
+            print("Fetch failed")
+        }
     }
     
     private func observeAuthState() {
@@ -38,7 +58,6 @@ final class UserModel: ObservableObject {
                 self.fetchBookmarkIDs()
             }
             .store(in: &cancellables)
-
     }
     
     func addBookmark(_ hackathon: Hackathon) {
